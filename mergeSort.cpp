1d0704edc2c41d249fbab3fdd1150177936a01cc
mitthhuu3110 <3110mithilesh@gmial.com>
2025-03-13 20:51:24 +0530

    adding mergeSort

---
+++ mergeSort.cpp
@@ -0,0 +1,46 @@
+class Solution{
+    public:
+    
+        void mergeArrays(vector<int> &arr, int left, int mid, int right){
+            vector<int> temp;
+    
+            int low = left;
+            int high = mid + 1;
+    
+            while(low <= mid && high <= right){
+                if(arr[low] <= arr[high]){
+                    temp.push_back(arr[low]);
+                    low++;
+                }else{
+                    temp.push_back(arr[high]);
+                    high++;
+                }
+            }
+    
+            while(low <= mid){
+                temp.push_back(arr[low]);
+                low++;
+            }
+    
+            while(high <= right){
+                temp.push_back(arr[high]);
+                high++;
+            }
+    
+            for (int i = left; i <= right; i++) {
+                arr[i] = temp[i - left];
+            }
+        }
+    
+        void mergeSort(vector<int>& arr, int left, int right) {
+            if(left >= right) return;
+    
+            int mid = left + (right - left) / 2;
+    
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+    
+            mergeArrays(arr, left, mid, right);
+        }
+    };
+    
