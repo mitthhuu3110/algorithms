e7d9bb2a5190de4eccd778e730c29b5f607d5bf0
Mithilesh_Perugu <103479865+mitthhuu3110@users.noreply.github.com>
2025-06-22 15:18:30 +0530

    Create no_of_occurrences.cpp

---
+++ basics/no_of_occurrences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int countFreq(vector<int>& arr, int target) {
+        // code here
+        //brute force -> O(n)
+        // int n = arr.size();
+        // int count = 0;
+        // for(int i = 0; i < n; i++){
+        //     if(arr[i] == target) count++;
+        // }
+        // return count;
+        
+        //optimized solution -> binary search
+        //good thought but will everyone get this thought ?? 
+        return upper_bound(arr.begin(), arr.end(), target) - lower_bound(arr.begin(), arr.end(), target);
+    }
+};
