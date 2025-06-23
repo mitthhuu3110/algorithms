82a64574f32e7359853e8227410c7ee9bcc6d14e
Mithilesh_Perugu <103479865+mitthhuu3110@users.noreply.github.com>
2025-06-23 23:46:28 +0530

    Create center_of_star_graph.cpp

---
+++ basics/center_of_star_graph.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        int n = edges.size();
+        int m = edges[0].size();
+
+        unordered_map<int, int> ump;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < 2; j++){
+                ump[edges[i][j]]++;
+            }
+        }
+
+        int maxi = 0;
+        int res;
+
+        for(auto it: ump){
+            maxi = max(maxi, it.second);
+            if(it.second == maxi) res = it.first;
+        }
+
+        return res;
+    }
+};
