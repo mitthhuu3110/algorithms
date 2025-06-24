75a470a13e2b524719584a79e666fda47562bef8
Mithilesh_Perugu <103479865+mitthhuu3110@users.noreply.github.com>
2025-06-24 12:22:37 +0530

    Create remove_duplicates.cpp

---
+++ basics/remove_duplicates.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        //brute force 
+        // set<int> temp;
+        // int n = nums.size();
+
+        // for(int i = 0; i < n; i++){
+        //     temp.insert(nums[i]);
+        // }
+
+        // int i = 0;
+
+        // for(int it: temp){
+        //     nums[i++] = it;
+        // }
+
+
+        // return temp.size();
+
+        //using 2 pointer methods 
+        if(nums.empty()) return 0;
+
+        int i = 0;
+        int n = nums.size();
+
+        for(int j = 1; j < n; j++){
+            if(nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i+1;
+    }   
+};
