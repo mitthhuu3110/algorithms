e2243d5a47d80ec0c104798025bf71530ff76892
Mithilesh_Perugu <103479865+mitthhuu3110@users.noreply.github.com>
2025-06-25 23:41:16 +0530

    Create maxSumSubarray.cpp

---
+++ basics/Arrays & it's patterns/maxSumSubarray.cpp
@@ -0,0 +1,24 @@
+// using sliding window & prefix sum simultaneously but simple solution 
+
+class Solution {
+  public:
+    int maximumSumSubarray(vector<int>& arr, int k) {
+        // code here
+        // how do you use prefix sum here ?? 
+        
+        int n = arr.size();
+        int windowSum = 0;
+        for(int i = 0; i < k; i++){
+            windowSum += arr[i];
+        }
+        int maxi = windowSum;
+        
+        
+        for(int i = k; i < n; i++){
+            windowSum += arr[i] - arr[i-k];
+            maxi = max(maxi, windowSum);
+        }
+        
+        return maxi;
+    }
+};
