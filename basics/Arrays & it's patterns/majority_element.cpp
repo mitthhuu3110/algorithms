2448962c30affee9f21d63a2f444d042ef532b52
Mithilesh_Perugu <103479865+mitthhuu3110@users.noreply.github.com>
2025-06-27 12:11:52 +0530

    Create majority_element.cpp

---
+++ basics/Arrays & it's patterns/majority_element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+
+        unordered_map<int, int> ump;
+
+        for(int i = 0; i < n; i++){
+            ump[nums[i]]++;
+        }
+
+        int maxi = INT_MIN;
+
+        for(auto it: ump){
+            if(it.second > floor(n/2)) maxi = it.first;
+        }
+
+        return maxi;
+    }
+};
