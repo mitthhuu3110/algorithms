bcffc9a11995665c40ede9d93c77c780166e93a4
Mithilesh_Perugu <103479865+mitthhuu3110@users.noreply.github.com>
2025-06-27 12:07:41 +0530

    Create longestSubstringWithoutRepeatingChar.cpp

---
+++ basics/Arrays & it's patterns/longestSubstringWithoutRepeatingChar.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+        unordered_map<char, int> ump;
+        int maxi = 0;
+
+        int left = 0, right = 0;
+
+        while(right < n){
+            ump[s[right]]++;
+            while(ump[s[right]] > 1){
+                ump[s[left]]--;
+                left++;
+            }
+            maxi = max(maxi, right - left + 1);
+            right++;
+        }
+
+        return maxi;
+    }
+};
