febce79ac10bfafcb0e8fbfca6e97da5b884b626
Mithilesh_Perugu <103479865+mitthhuu3110@users.noreply.github.com>
2025-06-30 21:39:58 +0530

    Update valid_parenthesis.cpp

--- basics/stacks_and_queues/valid_parenthesis.cpp
+++ basics/stacks_and_queues/valid_parenthesis.cpp
@@ -1,20 +1,48 @@
+// class Solution {
+// public:
+//     bool isValid(string s) {
+//         int n = s.length();
+//         stack<char> st;
+
+//         for(char ch: s){
+//             if(ch == '(' || ch == '{' || ch == '['){
+//                 st.push(ch);
+//             }else{
+//                 if(st.empty()) return false;
+//                 char top = st.top();
+//                 if((ch == ')' && top != '(') || (ch == '}' && top != '{') || (ch == ']' && top != '[')){
+//                     return false;
+//                 }
+//                 st.pop();
+//             }
+//         }
+//         return st.empty();
+//     }
+// };
+
+// solution having the best time complexity and space complexity 
+
 class Solution {
 public:
     bool isValid(string s) {
-        int n = s.length();
-        stack<char> st;
-
-        for(char ch: s){
-            if(ch == '(' || ch == '{' || ch == '['){
-                st.push(ch);
-            }else{
-                if(st.empty()) return false;
-                char top = st.top();
-                if((ch == ')' && top != '(') || (ch == '}' && top != '{') || (ch == ']' && top != '[')){
-                    return false;
-                }
+        stack <char> st;
+        int n=s.size();
+        
+        for(int i=0;i<n;i++){
+            if(st.empty())
+                st.push(s[i]);
+            
+            else if(st.top() == '(' && s[i] ==')')
                 st.pop();
-            }
+            
+            else if(st.top() == '{' && s[i] =='}')
+                st.pop();
+            
+            else if(st.top() == '[' && s[i] ==']')
+                st.pop();
+                
+            else
+                st.push(s[i]);
         }
         return st.empty();
     }
