6fafb7d5b9f857aacf29bd074827bcfe40bf52a0
Mithilesh_Perugu <103479865+mitthhuu3110@users.noreply.github.com>
2025-07-01 00:17:12 +0530

    Create next_greater_element.cpp

---
+++ basics/stacks_and_queues/next_greater_element.cpp
@@ -0,0 +1,17 @@
+//next greater element implementation using stack 
+
+vector<int> nextGreater(vector<int> &nums){
+	int n = nums.size();
+	vector<int> res(n, -1);
+	stack<int> st;
+	
+	for(int i = n-1; i >= 0; i--){
+		while(!st.empty() && st.top() <= nums[i]){
+			st.pop();
+		}
+		if(!st.empty()) res[i] = st.top();
+		st.push(nums[i]);
+	}
+	
+	return res;
+};
