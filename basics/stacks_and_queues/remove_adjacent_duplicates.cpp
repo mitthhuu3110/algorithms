5671b3d3e262e3a9ca13b6f74dc348d0ef01e45e
Mithilesh_Perugu <103479865+mitthhuu3110@users.noreply.github.com>
2025-07-01 00:16:47 +0530

    Create remove_adjacent_duplicates.cpp

---
+++ basics/stacks_and_queues/remove_adjacent_duplicates.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        int n = s.length();
+        stack<char> st;
+
+        for(int i = 0; i < n; i++){
+            if(!st.empty() && st.top() == s[i]){
+                st.pop();
+            }
+            else st.push(s[i]);
+        }
+
+        string res = "";
+        while(!st.empty()){
+            res += st.top();
+            st.pop();
+        }
+
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
