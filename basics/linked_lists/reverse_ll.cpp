ff75c493551e5de2a019d79891ed64c43ff73889
Mithilesh_Perugu <103479865+mitthhuu3110@users.noreply.github.com>
2025-07-03 15:37:38 +0530

    Update reverse_ll.cpp

--- basics/linked_lists/reverse_ll.cpp
+++ basics/linked_lists/reverse_ll.cpp
@@ -1,1 +1,18 @@
 // coming soon
+
+
+// reverse a linked list
+
+Node* reverseLL(Node* head){
+	Node* prev = nullptr;
+	Node* curr = head;
+	
+	while(Node && Node -> next){
+		Node* forward = head -> next;
+		curr -> next = prev;
+		prev = curr;
+		curr = forward;
+	}
+	
+	return prev;
+}
