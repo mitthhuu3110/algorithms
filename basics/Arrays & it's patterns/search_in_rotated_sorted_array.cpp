968ad09a3a55548a10f6a58ed19aa7237fdb6414
Mithilesh_Perugu <103479865+mitthhuu3110@users.noreply.github.com>
2025-07-22 12:35:32 +0530

    Create search_in_rotated_sorted_array.cpp

---
+++ basics/Arrays & it's patterns/search_in_rotated_sorted_array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l = 0, h = n-1;
+        while(l <= h){
+            int m = l + (h - l)/2;
+            if(nums[m] == target) {
+                return m;
+            }
+            // check whether the left half is sorted & after that continue binary search in the left half 
+            else if(nums[l] <= nums[m]){
+                if(nums[l] <= target && target <= nums[m]){
+                    h = m - 1;
+                }else l = m + 1;
+            }
+            // check whether the right half is sorted & after that continue binary search in the right half
+            else {
+                if(nums[m] <= target && target <= nums[h]){
+                    l = m + 1;
+                }else h = m - 1;
+            }
+        }
+
+        return -1;
+    }
+};
