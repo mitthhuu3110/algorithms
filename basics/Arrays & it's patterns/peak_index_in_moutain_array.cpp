fe15d12ed011ba1e3d935060fb08725a9934e1a3
Mithilesh_Perugu <103479865+mitthhuu3110@users.noreply.github.com>
2025-07-23 22:22:52 +0530

    Create peak_index_in_moutain_array.cpp

---
+++ basics/Arrays & it's patterns/peak_index_in_moutain_array.cpp
@@ -0,0 +1,24 @@
+// Solving it in the binary search method shows that you have a deeper understanding of the concept
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n = arr.size();
+
+        // for(int i = 1; i < n-1; i++){
+        //     if(arr[i] > arr[i+1] && arr[i] > arr[i-1]){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+        int l = 0;
+        int r = n-1;
+
+        while(l < r){
+            int m = l + (r - l)/2;
+            if(arr[m] > arr[m+1]){
+                r = m;
+            }else l = m+1;
+        }
+        return l;
+    }
+};
