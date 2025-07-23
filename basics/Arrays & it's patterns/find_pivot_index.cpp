a6c5bf5bb16ddc222b4ec834de1109750b6614e6
Mithilesh_Perugu <103479865+mitthhuu3110@users.noreply.github.com>
2025-07-23 22:40:58 +0530

    Create find_pivot_index.cpp

---
+++ basics/Arrays & it's patterns/find_pivot_index.cpp
@@ -0,0 +1,21 @@
+// question link -> https://leetcode.com/problems/find-pivot-index/description/
+
+class Solution {
+public:
+  int pivotindex(vector<int> &nums){
+    int n = nums.size();
+    int totSum = 0;
+    int leftSum = 0;
+
+    for(int i = 0; i < n; i++){
+      totSum += nums[i];
+    }
+
+    for(int i = 0; i < n; i++){
+      totSum -= nums[i];
+      if(totSum == leftSum) return i;
+      leftSum += nums[i];
+    }
+    return -1;
+  }
+};
