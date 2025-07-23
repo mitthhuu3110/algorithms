08192d12d253154ab9af8e07554b30fb67109629
Mithilesh_Perugu <103479865+mitthhuu3110@users.noreply.github.com>
2025-07-23 22:35:37 +0530

    Update 1st_and_last_occ.cpp

--- basics/Arrays & it's patterns/1st_and_last_occ.cpp
+++ basics/Arrays & it's patterns/1st_and_last_occ.cpp
@@ -1,3 +1,5 @@
+// question link -> https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+
 class Solution {
 public:
     vector<int> searchRange(vector<int>& arr, int target) {
