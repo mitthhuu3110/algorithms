1c071a84ed2293e1809cc6d31378881e9bb5795b
Mithilesh_Perugu <103479865+mitthhuu3110@users.noreply.github.com>
2025-07-27 11:49:53 +0530

    Update longest_repeating_characters_replacement.cpp

--- basics/Arrays & Strings/sliding-window/longest_repeating_characters_replacement.cpp
+++ basics/Arrays & Strings/sliding-window/longest_repeating_characters_replacement.cpp
@@ -25,3 +25,26 @@ public:
         
     }
 };
+
+
+// another solution by using vector instead of unordered map
+
+int characterReplacement(string s, int k) {
+    vector<int> freq(26, 0);
+    int maxFreq = 0, left = 0, result = 0;
+
+    for (int right = 0; right < s.length(); ++right) {
+        freq[s[right] - 'A']++;
+        maxFreq = max(maxFreq, freq[s[right] - 'A']);
+
+        int windowSize = right - left + 1;
+        if (windowSize - maxFreq > k) {
+            freq[s[left] - 'A']--;
+            left++;
+        }
+
+        result = max(result, right - left + 1);
+    }
+
+    return result;
+}
