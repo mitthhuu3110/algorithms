1101df65ecf1ccec416bdbec7bad9ea276dc54e0
Mithilesh Charan Perugu <103479865+mitthhuu3110@users.noreply.github.com>
2025-07-27 19:27:39 +0530

    Create longest_mountain_in_array.cpp

---
+++ basics/Arrays & Strings/2-pointer/longest_mountain_in_array.cpp
@@ -0,0 +1,25 @@
+// link to the problem -> https://leetcode.com/problems/longest-mountain-in-array/
+
+class Solution {
+public:
+
+    int longestMountain(vector<int>& arr) {
+        int n = arr.size();
+        if(n < 3) return 0;
+
+        int i = 1;
+        int maxi = 0;
+
+        while(i < n-1){
+            if(arr[i-1] < arr[i] && arr[i] > arr[i+1]){
+                int l = i-1, r = i+1;
+                while(l > 0 && arr[l-1] < arr[l]) l--;
+                while(r < n-1 && arr[r] > arr[r+1]) r++;
+                maxi = max(maxi, r - l + 1);
+                i = r;
+            }else i++;
+        }
+
+        return maxi;
+    }
+};
