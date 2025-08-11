8930ddc0848047a04f0bbb5ccba46a1ec7990bcf
mitthhuu3110 <3110mithilesh@gmail.com>
2025-08-11 23:26:32 +0530

    node

--- basics/linked_lists/reverse_ll.cpp
+++ basics/linked_lists/reverse_ll.cpp
@@ -1,5 +1,12 @@
 // coming soon
 
+struct Node {
+    int val;
+    Node *next;
+    Node() : val(0), next(nullptr) {}
+    Node(int x) : val(x), next(nullptr) {}
+    Node(int x, Node *next) : val(x), next(next) {}
+};
 
 // reverse a linked list
 #include <bits/stdc++.h>
